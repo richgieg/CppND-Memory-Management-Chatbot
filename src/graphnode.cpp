73ede84d7704e44f6fcd353db7ee12bd4ab1b117
Richard Gieg <richgieg@noreply.users.github.com>
2021-10-24 16:05:27 -0700

    More experimentation

--- src/graphnode.cpp
+++ src/graphnode.cpp
@@ -29,8 +29,7 @@ void GraphNode::AddEdgeToParentNode(GraphEdge *edge)
 
 void GraphNode::AddEdgeToChildNode(std::unique_ptr<GraphEdge> edge)
 {
-    _childEdges.push_back(edge.get());
-    _childEdges2.push_back(std::move(edge));
+    _childEdges.push_back(std::move(edge));
 }
 
 //// STUDENT CODE
@@ -54,7 +53,7 @@ GraphEdge *GraphNode::GetChildEdgeAtIndex(int index)
     //// STUDENT CODE
     ////
 
-    return _childEdges[index];
+    return _childEdges[index].get();
 
     ////
     //// EOF STUDENT CODE
